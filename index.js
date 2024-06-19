2a8650b04eca253868f8e6f1e5cb429494ff0cd6
Klymchuk Bohdan <okashichandesu@gmail.com>
2024-06-19 16:50:21 +0300

    Added scheduled posts. Moved access check to middleware

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { Telegraf, Markup } from 'telegraf'
 import { message } from 'telegraf/filters'
-import { fmt, link } from "telegraf/format"
+import { fmt, link, bold } from "telegraf/format"
+import cron from 'node-cron'
+import { JSONFilePreset } from 'lowdb/node'
 import crypto from 'crypto'
 import helpers from './src/helpers.js'
 
@@ -8,20 +10,44 @@ const token = Bun.env.TELEGRAM_BOT_TOKEN
 const channel = Bun.env.TELEGRAM_CHANNEL
 const allowedUsers = Bun.env.ALLOWED_USERS
 
+const initDb = { messages: [] }
+const db = await JSONFilePreset('db.json', initDb)
+
 const bot = new Telegraf(token)
 
+cron.schedule('0 5-23/2 * * *', () => {
+    if (db.data.messages.length === 0) return
 
+    bot.telegram.sendMediaGroup(channel, db.data.messages.at(0).message).then(() => {
+        console.log(`Sending: ${db.data.messages.at(0).booruId}.`)
+        db.update(({ messages }) => messages.shift())
+    })
+})
 
-bot.on(message('link_preview_options'), async (ctx) => {
+bot.use(async (ctx, next) => {
     const {
         message:
         {
             text: userMsg,
-            from: { id: userId }
+            from:
+            {
+                id: userId,
+                username: userUsername
+            }
         }
     } = ctx
 
-    if (!allowedUsers.includes(userId)) return
+    if (!allowedUsers.includes(userId)) {
+        console.log(`${userUsername}|${userId} sent a message: ${userMsg}`)
+        await next()
+        return
+    }
+
+    await next()
+})
+
+bot.on(message('link_preview_options'), async (ctx) => {
+    const { message: { text: userMsg } } = ctx
 
     const booruId = helpers.getDanbooruId(userMsg)
 
@@ -31,12 +57,14 @@ bot.on(message('link_preview_options'), async (ctx) => {
     }
 
     const post_callback = `post_${booruId}_${crypto.randomBytes(4).toString('hex')}`
+    const scheduled_callback = `schedule_${booruId}_${crypto.randomBytes(4).toString('hex')}`
 
     const message = await helpers.getMediaGroupMessage(booruId)
 
     ctx.sendMediaGroup(message).then(() => {
         ctx.reply('Запостити на каналі?', Markup.inlineKeyboard([
-            Markup.button.callback('Запостити', post_callback)
+            Markup.button.callback('Запостити', post_callback),
+            Markup.button.callback('Запланувати', scheduled_callback)
         ]))
     })
 
@@ -48,8 +76,41 @@ bot.on(message('link_preview_options'), async (ctx) => {
             })
         })
     })
+
+    bot.action(scheduled_callback, async (ctx) => {
+        ctx.deleteMessage().then(async () => {
+            if (db.data.messages.some(({ booruId: id }) => id === booruId)) {
+                ctx.reply(fmt`${bold`Пост вже заплановано!`}`)
+                return
+            }
+
+            db.update(({ messages }) => messages.push({ booruId, message }))
+
+            await db.write()
+
+            ctx.reply(fmt`${bold`Пост заплановано!`}`)
+        })
+    })
+})
+
+bot.command('scheduled', async (ctx) => {
+    if (db.data.messages.length === 0) {
+        ctx.reply('Запланованих постів немає.')
+        return
+    }
+
+    let scheduled = fmt`${bold`Заплановані пости:`}\n`
+
+    db.data.messages.forEach(({ booruId }) => {
+        const url = `https://danbooru.donmai.us/posts/${booruId}`
+
+        scheduled = fmt`${scheduled}[${link(`${booruId}`, url)}]\n`
+    })
+
+    ctx.reply(scheduled, { disable_web_page_preview: true })
 })
 
 bot.launch()
+
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
